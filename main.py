58f55adeec81b78ad5b543cdb7cfc6c4ab3d58a7
Divine-Beanbag <trevor@kasheycommand.com>
2025-11-01 21:51:47 -0500

    got the black screen working

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
-import pygame 
+import pygame
+pygame.init()
+from constants import *
+
+BLACK = (0, 0, 0, 1)
+
 
 def main():
+    
     print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.Surface.fill(screen, BLACK) #fill the screen with black color 
+        pygame.display.flip() #use display.flip to update the screen
+
 
 
 if __name__ == "__main__":
